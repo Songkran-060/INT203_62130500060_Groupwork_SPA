dff7f8e5ed78d6e7fa3596646aaed86305fc561a
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-04-19 17:15:59 +0700

    [ADD] About Views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about flex flex-col mx-auto text-center leading-loose max-w-2xl mt-12 bg-gray-200 shadow-2xl rounded-lg p-2">
+    <!-- Purpose -->
+    <p class="text-3xl">Purpose</p>
+    <p class="text-base">
+      เว็บไซต์นี้จัดทำขึ้นเพื่อบันเทิง และใช้ส่งงานในรายวิชา INT203 Client-side
+      Web Programming II ของคณะเทคโนโลยีสารสนเทศ สาขาเทคโนโลยีสารสนเทศ
+      มหาวิทยาลัยเทคโนโลยีสารสนเทศ
+    </p>
+    <!-- About Me -->
+    <p class="text-3xl">About Me</p>
+    <p class="text-base">Name: Pongpichet Tanthanawikrai</p>
+    <p class="text-base">StudentID: 62130500060</p>
+    <!-- Contact -->
+    <p class="text-3xl">Contact</p>
+    <a href="https://www.instagram.com/_sk.pt_/" class="text-base"><span class="fab fa-instagram"></span> _sk.pt_</a>
+    <p class="text-base"><span class="fas fa-phone-alt"></span> 0614018077</p>
   </div>
 </template>
