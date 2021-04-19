68153d9e8c89cc630c1188f4ee55a571fe091be8
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-04-19 20:34:01 +0700

    [ADD] ShowNovel Template

---
+++ src/components/ShowNovel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="grid grid-cols-2 auto-rows-auto gap-10 p-10">
+    <div v-for="n in novelList" :key="n.id">
+      <p>
+        <span>{{ n.id }}. {{ n.name }}</span> By <span>{{ n.writer }}</span>
+        <span>( ISBN: {{ n.isbn }} )</span>&nbsp;
+        <span class="far fa-edit text-green-400"></span>&nbsp;
+        <span class="far fa-trash-alt text-red-400"></span>
+      </p>
+      <div class="flex flex-row gap-8">
+        <img v-bind:src="n.imgSrc" class="" style="height:180px;" />
+        <p class="text-justify">{{ n.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    novelList: {
+      type: Array,
+      require: true,
+    },
+  },
+};
+</script>
---
+++ src/views/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <show-novel :novelList="novelList"></show-novel>
+  </div>
+</template>
+
+<script>
+
+import ShowNovel from "../components/ShowNovel.vue";
+export default {
+  components: {
+    ShowNovel
+  },
+  data() {
+    return {
+      dataLink: "http://localhost:5000/novel",
+      novelList:[]
+    };
+  },
+  methods:{
+    async fetchNovel(){
+      const res = await fetch("http://localhost:5000/novel")
+      const data = await res.json();
+      return data
+    },
+  },
+  async created(){
+      this.novelList =  await this.fetchNovel();
+    }
+};
+</script>
