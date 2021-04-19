62bd7ce1ec00cde9bf328d60ae969df9da26561d
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-04-19 17:15:01 +0700

    [EDIT] Navigation Bar To Center

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,23 +1,15 @@
 <template>
-  <div class="w-screen bg-DFF">
-    <div class="md:flex items-center py-4">
-      <div class="mr-12">
-        <h1 class="text-2xl text-black font-medium">
+  <div class="flex flex-row justify-center bg-DFF py-3">
+        <div class="text-2xl text-black font-medium mx-6 px-1">
           <router-link to="/">BL Novel</router-link>
-        </h1>
-      </div>
-      <div class="border-2 border-blue-400 rounded-md bg-DFF mx-12">
-        <h1 class="text-2xl text-black font-medium">
+        </div>
+        <div class="text-2xl text-black font-medium mx-6 border-2 border-blue-700 rounded-md bg-blue-200 px-1">
           <router-link to="/about">About</router-link>
-        </h1>
-      </div>
-      <div class="border-2 border-blue-400 rounded-md bg-DFF ml-12 ">
-        <h1 class="text-2xl text-black font-medium">
+        </div>
+        <div class="text-2xl text-black font-medium mx-6 border-2 border-blue-700 rounded-md bg-blue-200 px-1">
           <router-link to="/list">List Of Novel</router-link>
-        </h1>
-      </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
