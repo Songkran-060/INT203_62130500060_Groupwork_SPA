acd00f00b4f289a83e85b733d59abf70e0d6e8eb
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-04-19 16:24:15 +0700

    [EDIT] Navigation Bar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="w-screen bg-DFF">
+    <div class="md:flex items-center py-4">
+      <div class="mr-12">
+        <h1 class="text-2xl text-black font-medium">
+          <router-link to="/">BL Novel</router-link>
+        </h1>
+      </div>
+      <div class="border-2 border-blue-400 rounded-md bg-DFF mx-12">
+        <h1 class="text-2xl text-black font-medium">
+          <router-link to="/about">About</router-link>
+        </h1>
+      </div>
+      <div class="border-2 border-blue-400 rounded-md bg-DFF ml-12 ">
+        <h1 class="text-2xl text-black font-medium">
+          <router-link to="/list">List Of Novel</router-link>
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
